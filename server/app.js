f49c8b2fe7dc954c65bc10304e1b17fca26af180
achramadhans <littleworkshop.id@gmail.com>
2020-05-08 20:04:25 +0700

    'a'

--- server/app.js
+++ server/app.js
@@ -9,8 +9,8 @@ const errorHandler = require('./middlewares/errorHandler')
 const router = require('./routers')
 const PORT = process.env.PORT || 3000
 app.use(cors())
-app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(router)
 app.use(errorHandler)
 

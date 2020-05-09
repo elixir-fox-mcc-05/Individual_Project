8630eeb82cd64028b81ec6867a76791d34734c51
rahfathoni <rahfathoni@gmail.com>
2020-05-10 03:20:52 +0700

    repair readme server. Create client initial file

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-container fluid>
+            <b-row class="my-1" v-for="type in types" :key="type">
+                <b-col sm="3">
+                    <label :for="`type-${type}`">Type <code>{{ type }}</code>:</label>
+                </b-col>
+                <b-col sm="9">
+                    <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
+                </b-col>
+            </b-row>
+    </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            types: [
+                'email',
+                'password'
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

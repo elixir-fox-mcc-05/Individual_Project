3cd07b5c2648d36b76ffa2556999f76190155f0d
amirfaisalz <amirsakti27@gmail.com>
2020-05-09 15:53:34 +0700

    server done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const userRouter = require('./userRouter')
+const superHeroRouter = require('./superHeroRouter')
 
 router.use('/', userRouter)
+router.use('/', superHeroRouter)
 
 module.exports = router

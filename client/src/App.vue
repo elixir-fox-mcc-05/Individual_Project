2b0b2de7c73548d79d272cac663a71571d0cff28
adityapradipta <adityapradipta94@gmail.com>
2020-05-10 05:05:22 +0700

    client created

---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div v-if="currentPage == 'landingPage'">
+            <landingPage>
+
+            </landingPage>
+        </div>
+        <div v-else-if="currentPage == 'dashboardPage'">
+            <dashboardPage>
+
+            </dashboardPage>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import landingPage from './pages/landingPage'
+import dashboardPage from './pages/dashboardPage'
+
+export default {
+    name: 'App',
+    components: {
+        landingPage, dashboardPage
+    },
+    data() {
+        return {
+            currentPage: 'landingPage',
+        }
+    },
+    created() {
+        if(localStorage.token) {
+            this.currentPage = 'dashboardPage'
+        } else {
+            this.currentPage = 'landingPage'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

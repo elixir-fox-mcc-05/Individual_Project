e22ec9835874fe8f6928047c8c2682f4a5319de3
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-05-10 17:54:36 +0700

    com

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+
+
+    export default {
+        nama: 'App',
+        components: {
+            
+        },
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+            
+        }
+    };
+</script>
+
+<style scoped>
+</style>

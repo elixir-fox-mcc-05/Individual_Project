3773e6220171d71a06f2bb2677984b33b0fd47d3
AndreRGunawan <andre.graphica@gmail.com>
2020-05-10 17:32:47 +0700

    deploy attempt changing server details

--- movieApp_server/routes/userRouter.js
+++ movieApp_server/routes/userRouter.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const userController = require("../controllers/userController.js")
 
+router.get('/', function(req, res) {
+    res.status(200).json({
+        message: 'Home Domain - Top Reel App - Connected'
+    })
+})
+
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/googleSign', userController.googleSign)
